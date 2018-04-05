238258f3a0e87c8b4ff7b1c9c0344440c67f7c77
Tai Tran <taitran994@yahoo.com>
2018-04-05 17:02:59 -0400

    Added Keep Alive

--- ringo.py
+++ ringo.py
@@ -23,6 +23,8 @@ class Ringo:
         self.rtt_vector = {}
         # self.rtt_matrix = [[math.inf for i in range(n)] for j in range(n)]
         self.rtt_matrix = {}
+        self.activeRingos = set()
+        self.offlineRingos = set()
 
     # Ping
     def peer_discovery(self):
@@ -30,12 +32,11 @@ class Ringo:
         s.settimeout(3)  # 3 seconds
         # keep track of peers pinged so far
         peer_discovery_map = {}
-        while len(peer_discovery_map) < self.n-1 or min(peer_discovery_map.items()) < self.n-1:
+        while len(peer_discovery_map) < self.n - 1 or min(peer_discovery_map.items()) < self.n - 1:
             msg = "Peer Discovery/" + self.local_host + ":" + str(self.local_port)
             # starting ringo doesn't have a PoC; wait until there is a peer
             if self.poc_host == "0" and len(self.peers) == 0:
                 continue
-            
             # base case: ringo only has a PoC and no peers
             elif self.poc_host != "0" and len(self.peers) == 0:
                 addr = (self.poc_host, self.poc_port)
@@ -47,7 +48,7 @@ class Ringo:
                     peer_discovery_map[addr] = peers_discovered_by_peer
                 except socket.timeout:
                     print("Timed out in attempt to discover peers. Trying again")
-            
+
             # ping both poc and peers
             else:
                 for (peer_host, peer_port) in self.peers:
@@ -71,7 +72,6 @@ class Ringo:
         s.close()
         return
 
-    
     # listens to and sends from SERVER_PORT
     def listen(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -89,8 +89,11 @@ class Ringo:
 
                     if addr_of_peer != (self.local_host, self.local_port) and addr_of_peer not in self.peers:
                         self.peers.add(addr_of_peer)
+                        self.activeRingos.add(addr_of_peer)
                 server.sendto(str(len(self.peers)), addr)  # send back the number of peers it has
             elif "RTT" in data:
+                server.sendto(data, addr)
+            elif "Keep Alive" in data:
                 server.sendto(data, addr)
             else:
                 server.sendto(data, addr)
@@ -110,12 +113,10 @@ class Ringo:
                         # rtt_matrix[sorted_hosts.index(from_host)][sorted_hosts.index(host)] = sorted_hosts[host]
                         self.rtt_matrix[(from_host, from_port, to_host, to_port)] = rtt_vec[(to_host, to_port)]
 
-
     def initialize_rtt_vector(self):
         for peer in self.peers:
             self.rtt_vector[peer] = float("inf")
 
- 
     def calculate_rtt_vector(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.settimeout(2)
@@ -139,44 +140,70 @@ class Ringo:
             self.rtt_vector[peer] = time_diff / 3
         s.close()
 
+    def check_active_ringos (self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        msg = "Keep Alive"
+        while (1):
+            time.sleep(60)
+            for peer in self.offlineRingos:
+                try:
+                    _ = s.sendto(msg, peer)
+                    data_sent, _ = s.recvfrom(BUFFER_SIZE)
+                except socket.timeout:
+                    print("Server Timed Out")
+                    self.activeRingos.remove(peer)
+                    self.offlineRingos.add(peer)
+        return
+
+    def check_offline_ringos (self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        msg = "Keep Alive"
+        while (1):
+            time.sleep(15)
+            for peer in self.offlineRingos:
+                try:
+                    time.sleep(15)
+                    _ = s.sendto(msg, peer)
+                    data_sent, _ = s.recvfrom(BUFFER_SIZE)
+                    self.offlineRingos.remove(peer)
+                    self.activeRingos.add(peer)
+                except socket.timeout:
+                    print("Server Time Out")
+        return
 
     # send to SERVER PORT or create a new port for that?
     def send_rtt_vectors(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.settimeout(2)
-        msg = self.get_rtt_vector_msg()
         self.initialize_rtt_matrix()
         for peer in self.peers:
             try:
-                time.sleep(0.5)
+                msg = "Keep Alive"
                 _ = s.sendto(msg, peer)
                 data_sent, _ = s.recvfrom(BUFFER_SIZE)
             # handle: try again
             except socket.timeout:
-                print("Timed out. Trying again")
+                self.offlineRingo
         while len(self.rtt_matrix) != self.n * self.n:
             time.sleep(0.2)
         return
-
 
     def get_rtt_vector_msg(self):
         rtt_vector = self.rtt_vector
         curr_ringo_addr = (socket.gethostname(), self.local_port)
         rtt_vector[curr_ringo_addr] = 0
-        msg = "rtt_vectors/" + socket.gethostname() + ":" + str(self.local_port)  + "/"
+        msg = "rtt_vectors/" + socket.gethostname() + ":" + str(self.local_port) + "/"
         for peer in rtt_vector.keys():
             msg += peer[0] + ":" + str(peer[1]) + "=" + str(rtt_vector[peer]) + ","
         if msg:
             msg = msg[:-1]  # remove last comma
         return msg
 
-
     def initialize_rtt_matrix(self):
         rtt_vector = self.rtt_vector
         rtt_vector[socket.gethostname(), self.local_port] = 0
         for (host, port) in rtt_vector:
             self.rtt_matrix[(socket.gethostname(), self.local_port, host, port)] = self.rtt_vector[(host, port)]
-
 
     def print_rtt_matrix(self):
         all_ringos = [(socket.gethostname(), self.local_port)] + list(self.peers)
@@ -191,11 +218,12 @@ class Ringo:
             i = 0
             for to_host, to_port in sorted_ringos:
                 if i == 0:
-                    sys.stdout.write("      " + str(round(self.rtt_matrix[(from_host, from_port, to_host, to_port)], 2)) + "      ")
+                    sys.stdout.write(
+                        "      " + str(round(self.rtt_matrix[(from_host, from_port, to_host, to_port)], 2)) + "      ")
                 else:
-                    sys.stdout.write(str(round(self.rtt_matrix[(from_host, from_port, to_host, to_port)], 2)) + "      ")
+                    sys.stdout.write(
+                        str(round(self.rtt_matrix[(from_host, from_port, to_host, to_port)], 2)) + "      ")
             sys.stdout.write("\n")
-
 
     def make_rtt_matrix_symmetric(self):
         hosts = [(socket.gethostbyname(socket.gethostname()), self.local_port)] + list(self.peers)
@@ -207,7 +235,6 @@ class Ringo:
                 self.rtt_matrix[(host1[0], host2[0])] = average
                 self.rtt_matrix[(host2[0], host1[0])] = average
 
-
     def optimal_path(self):
         hosts = []
         for peer in self.peers:
@@ -218,9 +245,10 @@ class Ringo:
         for order in possible_orders:
             # starting host must be current ringo
             total_rtt = 0
-            if order[0] == socket.gethostname() + ":" + str(self.local_port) and order[-1] == socket.gethostname() + ":" + str(self.local_port):
+            if order[0] == socket.gethostname() + ":" + str(self.local_port) and order[
+                -1] == socket.gethostname() + ":" + str(self.local_port):
                 for i in range(len(order) - 1):
-                    pair = order[i:i+2]
+                    pair = order[i:i + 2]
                     from_ringo = pair[0].split(":")
                     to_ringo = pair[1].split(":")
                     total_rtt += self.rtt_matrix[(from_ringo[0], int(from_ringo[1]), to_ringo[0], int(to_ringo[1]))]
@@ -254,11 +282,15 @@ def main():
     ringo = Ringo(flag, local_port, poc_host, poc_port, n)
     help_others = threading.Thread(target=ringo.listen, args=())
     help_others.start()
-    ringo.peer_discovery()    
+    ringo.peer_discovery()
     ringo.calculate_rtt_vector()
     ringo.send_rtt_vectors()
     total_rtt, optimal_path = ringo.optimal_path()
-    
+    active_ringos = threading.Thread(target=ringo.check_active_ringos, args=())
+    active_ringos.start()
+    offline_ringos = threading.Thread(target=ringo.check_offline_ringos, args=())
+    offline_ringos.start()
+
     while (1):
         command_input = raw_input("Ringo command: ")
         if command_input == "show-matrix":
@@ -274,5 +306,6 @@ def main():
             print("Please input one of the follow commands: <show-matrix>, <show-ring>, <disconnect>")
 
     # help_others.join()
+
 
 main()
